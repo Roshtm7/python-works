1067b559c83fd27821c32a4c3a0c8db3a08ec8af
Roshan <mohammmedroshantm7@gmail.com>
2024-09-29 22:43:57 +0530

    new python works

--- oops/methodOverriding.py
+++ oops/methodOverriding.py
@@ -1,18 +1,31 @@
+# class Parent:
+#     def bike(self):
+#         print("glamour")
+
+#     def mobile(self):
+#         print("apple")
+
+# class Child(Parent):
+#     def mobile(self):
+#         print("samsung")
+
+#     def bike(self):
+#         print("bullet")
+# child_instance=Child()
+# child_instance.bike()
+# child_instance.mobile()
+
+
 class Parent:
-    def bike(self):
-        print("glamour")
-
-    def mobile(self):
-        print("apple")
-
+    def properties(self):
+        print("5kg gold,1bmw car,5000sq feet house")
+    def marriage_with(self):
+        print("roshan")
 class Child(Parent):
-    def mobile(self):
-        print("samsung")
-
-    def bike(self):
-        print("bullet")
+    pass
 child_instance=Child()
-child_instance.bike()
-child_instance.mobile()
+child_instance.properties()
+child_instance.marriage_with()
+
 
 
