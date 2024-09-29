9fc47beec5b61cbebe17d5b52d0fbdceb9466129
Roshan <mohammmedroshantm7@gmail.com>
2024-09-29 18:41:32 +0530

    new python files

--- oops/funOverloading.py
+++ oops/funOverloading.py
@@ -2,10 +2,27 @@
 #     return n1+n2+n3
 # print (add_numbers(100,200,300))
 
-def add_numbers(*args):
-    print (sum(args))
+# def add_numbers(*args):
+    # print (sum(args))                             *args = (argument tuple)
+                                                #  **kwargs = (keyword arguments dictionary)
 
-add_numbers(10,20)
-add_numbers(10,20,30)
-add_numbers(10,20,30,40)
-add_numbers(10,20,30,40,50)
+# add_numbers(10,20)
+# add_numbers(10,20,30)
+# add_numbers(10,20,30,40)
+# add_numbers(10,20,30,40,50)
+
+number = 10.5
+print(isinstance(number,(int,float)))
+
+
+# def get_person(*args):
+#     print(args[0])
+#     print(args[1])
+# get_person("hari","tvm","kakkanad")
+
+def get_person(**kwargs):
+    print(kwargs.get("name"))
+    print(kwargs.get("w_place"))
+get_person(name="hari",w_place="tvm",n_place="kakkanad")
+
+
